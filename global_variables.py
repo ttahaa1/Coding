6921884ae8fcbbf401211d314af70729156d53ab
speed <153678863+ttahaa1@users.noreply.github.com>
2024-03-26 02:53:11 +0200

    Update global_variables.py

--- global_variables.py
+++ global_variables.py
@@ -66,15 +66,15 @@ class GlobalVariables(Enum):
     """
 
     # Bot related variables
-    BOT_API_ID: str = "----"  # From telegram id handler
-    BOT_API_HASH: str = "----------------------------"  # from telegram id handler
-    BOT_API_TOKEN: str = "---------------------------------"  # from bot father
+    BOT_API_ID: str = "24677256"  # From telegram id handler
+    BOT_API_HASH: str = "ef4f700f61fd3e6dac816738d62ba375"  # from telegram id handler
+    BOT_API_TOKEN: str = "6841560275:AAHsxj7tNXqyZzB1YaiSbCPrRuE3O4h21XQ"  # from bot father
     PLATFORM: str = platform.system().lower()
 
     # Auther and bot version related variables
-    BOT_AUTHORS: str = "@RULEROFCODES & @reverse_master"
-    BOT_AVAILABILITY: str = "FREE_ALPHA_VERSION"
-    BOT_NAME: str = "@reverse_master_bot"
+    BOT_AUTHORS: str = "@ELPAPPA1 & @KawKabMod"
+    BOT_AVAILABILITY: str = " #Free_Palestine 🇵🇸 "
+    BOT_NAME: str = "@Coding_KawKab_Bot"
     BOT_VERSION: float = 0.7
 
     # Temporary Directories related variables
@@ -94,7 +94,6 @@ class GlobalVariables(Enum):
     ALLOWED_UPLOAD_MBS: int = 100
     BOT_STATICS_MESSAGE: str = f"**__[BOT STATICS]__**\n\n**--AUTHOR--**  **__-> {BOT_AUTHORS}__**\n\n" \
                                f"**--BOT_VERSION--**  **__-> {BOT_VERSION}__**\n" \
-                               f"\n**--AVAILABLE_AS--**  **__-> {BOT_AVAILABILITY}__**"
 
     # Link Shorter options
     LINK_SHORTER_API_KEY: str = "edc01b5d741ff32f6c2d175a70dc6447"
@@ -230,11 +229,11 @@ class GlobalVariables(Enum):
                             "Kindly reach out to the developer for more information regarding the error. "
     HELP_KEYBOARD_MENU: list = [
         [
-            ('Telegram Channel', 'https://t.me/RulerKingCodes', 'url'),
-            ('ReverseMaster', 'https://t.me/reverse_master', 'url')
+            ('Owner', 'https://t.me/ELPAPPA1', 'url'),
+            ('Creator', 'https://t.me/l_s_I_I', 'url')
         ],
         [
-            ('RULEROFCODES', 'https://t.me/RULEROFCODES', 'url')
+            ('KawKab Mod 🇵🇸', 'https://t.me/KawkabMod', 'url')
         ],
     ]
 
